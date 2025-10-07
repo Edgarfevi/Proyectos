adf15ef504488225110257bdfdf2423292ebfe15
Edgarfevi <edgarfevi@outlook.es>
2025-10-07 20:13:40 +0200

    algunos cambios

---
+++ Proyectos_python/Mis_cosas/calendario.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+
+# Leer el archivo Excel
+df = pd.read_excel('/home/edgar/Documentos/GitHub/Proyectos/Proyectos_python/Mis_cosas/nombre.xls')  # o .xlsx
+
+# Ver las primeras filas completas
+print(df)
