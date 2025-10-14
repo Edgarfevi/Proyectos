2998aaa84be23f90df01376ee1f0784651e3de7b
Edgarfevi <edgarfevi@outlook.es>
2025-10-14 17:27:11 +0200

    algunos cambios para el calendario

--- Proyectos_python/Mis_cosas/calendario.py
+++ Proyectos_python/Mis_cosas/calendario.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
-
+from ics import Calendar, Event
 # Leer el archivo Excel
 df = pd.read_excel('/home/edgar/Documentos/GitHub/Proyectos/Proyectos_python/Mis_cosas/nombre.xls')  # o .xlsx
 
@@ -43,3 +43,37 @@ df = pd.DataFrame.from_dict(calendario, orient='index')
 print(df)
 
 df.to_csv('calendario.csv', index=False)
+
+import pandas as pd
+from datetime import datetime
+
+# Supongamos que tu DataFrame se llama df
+# df = pd.read_csv("tu_csv.csv")  # si lo tienes en CSV
+
+# Abrir archivo ICS para escribir
+with open("calendario.ics", "w") as f:
+    f.write("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Mi Calendario//EN\n")
+
+    for index, row in df.iterrows():
+        f.write("BEGIN:VEVENT\n")
+        f.write(f"UID:{index}\n")  # ID único por fila
+        f.write(f"SUMMARY:{row['Subject']}\n")
+        
+        # Convertir Start Date + Start Time a formato 24h para ICS
+        if row['Start Time']:  # si tiene hora
+            start = datetime.strptime(row['Start Date'] + " " + row['Start Time'], "%d/%m/%Y %H:%M")
+            end = datetime.strptime(row['End Date'] + " " + row['End Time'], "%d/%m/%Y %H:%M")
+            f.write(f"DTSTART:{start.strftime('%Y%m%dT%H%M%S')}\n")
+            f.write(f"DTEND:{end.strftime('%Y%m%dT%H%M%S')}\n")
+        else:  # Evento de todo el día
+            start = datetime.strptime(row['Start Date'], "%d/%m/%Y")
+            end = datetime.strptime(row['End Date'], "%d/%m/%Y")
+            f.write(f"DTSTART;VALUE=DATE:{start.strftime('%Y%m%d')}\n")
+            f.write(f"DTEND;VALUE=DATE:{end.strftime('%Y%m%d')}\n")
+        
+        f.write(f"LOCATION:{row['Location']}\n")
+        f.write(f"DESCRIPTION:\n")  # puedes añadir row['Description'] si tienes
+        f.write(f"STATUS:CONFIRMED\n")
+        f.write("END:VEVENT\n")
+
+    f.write("END:VCALENDAR")
