2b558e95af1c13797718adb5b87c17e4dd708f58
Edgarfevi <edgarfevi@outlook.es>
2025-10-31 11:54:47 +0100

    grafcas

---
+++ Proyectos_python/TEIII/archivos_py/graficas.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Abre el archivo Excel
+df = pd.read_excel("/home/edgar/OneDrivePersonal/Universidad/excell.xlsx")
+
+# Muestra las primeras filas
+print(df.head())
+
+
+angulos_45=[]
+angulos_30=[]
+paralela_30=[]
+paralela_45=[]
+perpendicular_30=[]
+perpendicular_45=[]
+
+for i in range(0,16):
+    angulos_45.append(df.iloc[i+1,1])
+    paralela_45.append(df.iloc[i+1,5])
+    perpendicular_45.append(df.iloc[i+1,6])
+
+
+plt.plot(angulos_45,paralela_45,"o",label="horizontal")
+plt.plot(angulos_45,perpendicular_45,"o",color="r",label="vertical")
+plt.title(f"GRÁFICA INTENSIDAD FRENTE ÁNGULO")
+plt.xlabel(r"Angulo $\alpha =45º$")
+plt.ylabel(f"Intensidad (I)")
+plt.legend()
+plt.grid()
+plt.savefig("grafico.png")
+plt.show()
