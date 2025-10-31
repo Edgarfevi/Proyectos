d265d1bcc82d2617a82e4a38e8f7c9ec8966718c
Edgarfevi <edgarfevi@outlook.es>
2025-10-31 12:20:42 +0100

    graficas hechas

--- Proyectos_python/TEIII/archivos_py/graficas.py
+++ Proyectos_python/TEIII/archivos_py/graficas.py
@@ -20,6 +20,10 @@ for i in range(0,16):
     angulos_45.append(df.iloc[i+1,1])
     paralela_45.append(df.iloc[i+1,5])
     perpendicular_45.append(df.iloc[i+1,6])
+    if df.iloc[i+16,1] != "NaN":
+        angulos_30.append(df.iloc[i+16,1])
+        paralela_30.append(df.iloc[i+16,5])
+        perpendicular_30.append(df.iloc[i+16,6])
 
 
 plt.plot(angulos_45,paralela_45,"o",label="horizontal")
@@ -29,5 +33,15 @@ plt.xlabel(r"Angulo $\alpha =45º$")
 plt.ylabel(f"Intensidad (I)")
 plt.legend()
 plt.grid()
-plt.savefig("grafico.png")
-plt.show()
+plt.savefig("/home/edgar/GitHub/Proyectos/Proyectos_python/TEIII/graficas/grafico_45.png")
+
+
+plt.figure()
+plt.plot(angulos_30, paralela_30, "o", label="horizontal")
+plt.plot(angulos_30, perpendicular_30, "o", color="r", label="vertical")
+plt.title(f"GRÁFICA INTENSIDAD FRENTE ÁNGULO")
+plt.xlabel(r"Angulo $\alpha =30º$")
+plt.ylabel(f"Intensidad (I)")
+plt.legend()
+plt.grid()
+plt.savefig("/home/edgar/GitHub/Proyectos/Proyectos_python/TEIII/graficas/grafico_30.png")
