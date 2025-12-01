5d28eb20b1c28d40d6a3eaaf09f12c5d43eb61b4
Edgarfevi <edgarfevi@outlook.es>
2025-12-01 14:46:26 +0100

    comit

--- Proyectos_python/FISICA/MNAF2025/archivos_py/__init__.py
+++ Proyectos_python/FISICA/MNAF2025/archivos_py/__init__.py
@@ -1,1 +1,19 @@
-print("Edgar Fernández Vigil")
+import time
+import asyncio
+
+
+async def tarea1():
+    print("Tarea iniciada...")
+    await asyncio.sleep(2)
+    print("Tarea completada.")
+
+def tarea2():
+    print("Tarea 2 iniciada...")
+    time.sleep(1)
+    print("Tarea 2 completada.")
+
+async def main():
+    print("Starting main function...")
+    await tarea1()
+    tarea2()
+    print("Main function completed.")
