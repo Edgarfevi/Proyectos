cee5a452bc1ab1b8ee3e9f14a2d1f7ce22a899d6
Edgarfevi <edgarfevi@outlook.es>
2026-02-17 12:00:51 +0100

    cambios en las clases de MNAF2025

--- Proyectos_python/FISICA/MNAF2025/Mi_MNAF/Segundo_cuatri/propagacion_ondas_1D.py
+++ Proyectos_python/FISICA/MNAF2025/Mi_MNAF/Segundo_cuatri/propagacion_ondas_1D.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-class Propagacion_ondas:
+class Propagacion_ondas_1D:
     """
-    Docstring for Propagacion_ondas
+    Docstring for Propagacion_ondas_1D class.
     """
 
     def __init__(
@@ -18,8 +18,8 @@ class Propagacion_ondas:
         E0=1,
         kp=250,
         distancia_simulacion=10,
-        er1=1,
-        er2=4
+        er1=16,
+        er2=49
     ):
         self.velocidad_prop = c
         self.espaciado_mallado = espaciado_mallado
@@ -86,11 +86,11 @@ class Propagacion_ondas:
         self.Ey[1:] = self.Ey[1:] - self.E_r()[1:]*(self.Hz[1:] - self.Hz[:-1])
         self.Ey[self.kp] = self.Ey[self.kp] + self.Pulso(t)
         self.Ey[0] = self.variable_apoyo1[0]
-        self.variable_apoyo1[:-1] = self.variable_apoyo2[1:]
-        self.variable_apoyo2[-1] = self.Ey[1]
+        self.variable_apoyo1[:-1] = self.variable_apoyo1[1:]
+        self.variable_apoyo1[-1] = self.Ey[1]
         self.Ey[-1] = self.variable_apoyo3[-1]
-        self.variable_apoyo3[1:] = self.variable_apoyo4[:-1]
-        self.variable_apoyo4[0] = self.Ey[-2]
+        self.variable_apoyo3[1:] = self.variable_apoyo3[:-1]
+        self.variable_apoyo3[0] = self.Ey[-2]
         self.Hz[:-1] = self.Hz[:-1] - 0.5 *(self.Ey[1:] - self.Ey[:-1])
 
     def generar_animacion(self):
@@ -139,11 +139,106 @@ class Propagacion_ondas:
 
 
 
-# ...existing code...
-Prueba = Propagacion_ondas()
+class Propagacion_ondas_2D(Propagacion_ondas_1D):
+    def __init__(
+        self,
+        c=3e8,
+        espaciado_mallado=10e-9,
+        puntos_mallado=401,
+        simulacion_pasos=10000,
+        delta_xp=40e-9,
+        E0=1,
+        kp=200,
+        distancia_simulacion=10,
+        er1=1,
+        er2=4,
+        lp=200
+    ):
+        super().__init__(
+            c=c,
+            espaciado_mallado=espaciado_mallado,
+            puntos_mallado=puntos_mallado,
+            simulacion_pasos=simulacion_pasos,
+            delta_xp=delta_xp,
+            E0=E0,
+            kp=kp,
+            distancia_simulacion=distancia_simulacion,
+            er1=er1,
+            er2=er2
+        )
+    
+        self.malla_y = None
+        self.Ez = None
+        self.Hx = None
+        self.Hy = None
+        self.lp = lp
 
-print(Prueba.E_r())
-Prueba.generar_animacion()
-# ...existing code...
+    def generar_mallado(self):
+        super().generar_mallado()
+        self.malla_y = np.linspace(0, self.num_puntos * self.espaciado_mallado, self.num_puntos)
+        print(self.malla_x.shape == self.malla_y.shape)
 
+        Malla_Y, Malla_X = np.meshgrid(self.malla_y, self.malla_x)
+        self.malla_x = Malla_X
+        self.malla_y = Malla_Y
+
+        return Malla_X, Malla_Y
+    
+    def Onda_2D(self):
+        self.Ez = np.zeros((self.num_puntos, self.num_puntos))
+        self.Hx = np.zeros((self.num_puntos, self.num_puntos))
+        self.Hy = np.zeros((self.num_puntos, self.num_puntos))
+
+    def Pulo_2D(self, t):
+        return self.E0 * np.exp(-((t - self.top) ** 2) / (2 * (self.delta_tp ** 2)))
+    
+    def Propagacion_campos_2D(self, t):
+        self.Ez[1:, 1:] = self.Ez[1:, 1:] + 0.5 * (self.Hy[1:, 1:] - self.Hy[:-1, 1:]) - 0.5*(self.Hx[1:, 1:] - self.Hx[1:, :-1])
+        self.Ez[self.kp, self.lp] = self.Pulo_2D(t)
+        self.Hx[:, :-1] = self.Hx[:, :-1] - 0.5 * (self.Ez[:, 1:] - self.Ez[:, :-1])
+        self.Hy[:-1, :] = self.Hy[:-1, :] + 0.5 * (self.Ez[1:, :] - self.Ez[:-1, :])
+
+    def generar_animacion_2D(self):
+        if self.malla_x is None or self.malla_y is None or self.malla_t is None:
+            self.generar_mallado()
+
+        if self.kp < 0 or self.kp >= self.num_puntos:
+            raise ValueError(f"kp fuera de rango: {self.kp} (0..{self.num_puntos-1})")
+
+        self.Onda_2D()
+        niveles=np.linspace(-0.1,0.1,21)
+        fig, ax = plt.subplots()
+        cs = ax.contourf(self.malla_x, self.malla_y, np.clip(self.Ez, -0.1, 0.1), niveles, cmap='hot')
+        barracolor = plt.colorbar(cs)
+
+
+        total_frames = self.simulacion_pasos // self.distancia_simulacion
+
+        def animacion(frame):
+            inicio = frame * self.distancia_simulacion
+            fin = min(inicio + self.distancia_simulacion, self.simulacion_pasos)
+            for t in range(int(inicio), int(fin)):
+                tiempo = t * self.paso_temporal
+                self.Propagacion_campos_2D(tiempo)
+            ax.cla()
+            ax.contourf(self.malla_x, self.malla_y, np.clip(self.Ez, -0.1, 0.1), niveles, cmap='hot')
+            ax.set_title(f'Propagación de Onda Electromagnética en 2D -- t = {(frame * self.distancia_simulacion * self.paso_temporal * 10**15):.2f} fs')
+            
+            return None
+
+
+        self.anim = FuncAnimation(
+            fig, animacion, frames=total_frames, interval=10, blit=False, repeat=False
+        )
+        plt.show()
+
+"""
+Propagacion_ondas_2D_instance = Propagacion_ondas_2D()
+print(Propagacion_ondas_2D_instance.delta_tp)
+
+Propagacion_ondas_2D_instance.generar_animacion_2D()
+
+"""
+Propagacion_ondas_1D_instance = Propagacion_ondas_1D()
+Propagacion_ondas_1D_instance.generar_animacion()
 
